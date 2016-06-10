55cf2461afad1e7583d98e9a00baeb021effe3d9
krisheinrich <krisheinrich@gmail.com>
2016-06-10 10:16:44 -0700

    code cleanup

--- js/number-theory.js
+++ js/number-theory.js
@@ -1,7 +1,18 @@
-Number.prototype.factorial = function (n) {
+// Extend functionality of Number object
+
+var getFactorsArray = function (n) {
+  var factors = [], f = 1;
+  do {
+    if (n %f == 0) factors.push(f);
+    f++;
+  } while (f <= n);
+  return factors;
+};
+
+var factorial = function (n) {
   // Check input: 1 <= n <= 20
   if (!Number.isInteger(n)) {
-    // send user warning message later
+    // send user warning message
     return 0;
   } else if (n > 20) {
     // warn user number too high
